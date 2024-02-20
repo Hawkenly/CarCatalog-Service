66c8a6d05cf662c89605b81111ec2069f2204a91
Andrei <az4014529@gmail.com>
2024-02-20 10:55:58 +0300

    committed

--- src/main/java/org/example/lab1/model/CarModels.java
+++ src/main/java/org/example/lab1/model/CarModels.java
@@ -1,3 +1,17 @@
 package org.example.lab1.model;
 
-public record CarModels(String response){}
+public class CarModels{
+    private String response;
+
+    public CarModels(String response){
+        this.response=response;
+    }
+
+    public String getResponse(){
+        return response;
+    }
+
+    public void setResponse(String response){
+        this.response=response;
+    }
+}
