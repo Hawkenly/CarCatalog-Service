8d1fc3978abc7ef684d72c24a33d8f9aa6ba7f33
Andrei <az4014529@gmail.com>
2024-04-16 08:39:09 +0300

    committed

--- src/main/java/org/example/carcatalog/service/CarModelService.java
+++ src/main/java/org/example/carcatalog/service/CarModelService.java
@@ -66,6 +66,7 @@ public class CarModelService {
             car.removeModel(model);
         }
         carModelRepository.delete(model);
+
         modelSimpleCache.remove(id.toString());
     }
 }
